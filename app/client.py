fd73767125091636266dff3aedfa101918a555c5
sahilshah6390-star <sahilshah6390@gmail.com>
2026-01-03 14:31:02 +0530

    Add files via upload

---
+++ app/client.py
@@ -0,0 +1,11 @@
+# app/client.py
+
+from pyrogram import Client
+from config import API_ID, API_HASH, BOT_TOKEN
+
+app = Client(
+    "kasukabe_hosting",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
